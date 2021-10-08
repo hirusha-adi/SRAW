f88950baf26d3a16e1126981681123af2929c3c7
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2021-10-08 14:00:48 +0530

    Base64 encoding and decoding

--- sraw/misc.py
+++ sraw/misc.py
@@ -40,13 +40,34 @@ class Misc:
         
         return (cont['title'], cont['author'], cont['lyrics'], cont['thumbnail']['genius'], cont['links']['genius'])
 
-
-
-
     
+    def base64(self, mode: str = "encode", query: str = None):
+        params_query = {}
+        linka = "https://some-random-api.ml/base64"
 
+        if mode.lower().startswith('e'):
+            params_query['encode'] = query
+        
+        if mode.lower().startswith('d'):
+            params_query['decode'] = query
 
+        query_string = parse.urlencode(params_query)
 
+        req = requests.get(f"{linka}?{query_string}")
+
+        if 300 > req.status_code >= 200:
+            cont = req.json()
+        else:
+            raise errors.BadResponseFromAPI
+        
+        if self.rjson == True:
+            return cont
+        
+        if mode.lower().startswith('e'):
+            return cont['base64']
+        
+        if mode.lower().startswith('d'):
+            return cont['text']
 
 
 
